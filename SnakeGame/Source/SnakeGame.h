4fb2210cd01f071a1b69b5cc016c779e5e97a24a
Mikapo <30571759+Mikapo@users.noreply.github.com>
2022-07-02 14:55:32 +0300

    added more helpful comments to header

--- SnakeGame/Source/SnakeGame.h
+++ SnakeGame/Source/SnakeGame.h
@@ -10,8 +10,13 @@ public:
 	void Start();
 
 private:
+	// gets called on begining of the game
 	void BeginPlay();
+
+	// gets called every frame
 	void Tick();
+
+	// gets called on input
 	void OnInput(int32_t Key, int32_t Scancode, int32_t Action, int32_t Mods);
 
 	Application Window;
